158bea96b2f35a5f006d8f29af87a603ca0c6067
Anudeep varma <datlanagaanudeep@gmail.com>
2024-01-11 18:09:15 +0530

    Initial commit

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 const parent = React.createElement("div",{id: "parent"},[
-React.createElement("div",{id: "child"},
+React.createElement("div",{id: "child1"},
 [React.createElement("h1",{},"I am a h1 tag"),
 React.createElement("h2",{},"I am a h2 tag"),
  ]),
